5108a4916c6b5dbb20d70584a82ccc4f1b46aa0e
tadahmen <tadahmen@gmail.com>
2016-11-19 18:00:07 +0100

    added loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: "./src/app.js",
   output: {
@@ -11,6 +13,13 @@ module.exports = {
       {
         test: /\.scss$/,
         loaders: ["style", "css", "sass"]
+      },
+      {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          ]
       }
     ]
   }
